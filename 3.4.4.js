c4d829e121bc601d5705262eecf05fb549213df5
VetN <122635397+VetN@users.noreply.github.com>
2023-04-22 15:00:28 +0300

    add wi-fi

--- 3.4.4.js
+++ 3.4.4.js
@@ -1,32 +1,35 @@
 // создаем функцию- конструктор объектов
-function SmartHome(name, guarantee, power, color) {
+function SmartHome(name, guarantee, power, color, wifi) {
     this.name = name,
     this.series = 'Comfort',
     this.guarantee = guarantee,
     this.power = power,
     this.color = color,
-    this.wifi = true
+    this.wifi = wifi
 }
-
+SmartHome.prototype.guarantee = function () {
+    if (this.guarantee === 'standart') {
+        console.log(`${this.guarantee} warranty 12 month`)
+    } else if (this.guarantee === 'extended') {
+        console.log(`${this.guarantee} warranty 24 month`)
+    } else {
+        console.log('Without warranty')
+    }
+}
 SmartHome.prototype.wifi_on = function () {
-    this.wifi = true;
-    console.log(`${this.name} connecting to wi-fi: on`)
+   this.wifi == 'on' ?  console.log(`${this.name} connecting to wi-fi: on`) : console.log(`${this.name} connecting to wi-fi: off`);
 }  
 
-SmartHome.prototype.wifi_off = function () {
-    this.wifi = false;
-    console.log(`${this.name} connecting to wi-fi: off`)
-}
 SmartHome.prototype.description = function () {
-    console.log(`Product name: ${this.name}, series: ${this.series}`);
-    console.log(`Product warranty: ${this.guarantee}, color ${this.color}`);
+    console.log(`Product name: ${this.name}, series: ${this.series}, color ${this.color}`);
     if (this.power === 220) {
         console.log(`The ${this.name} is powered by 220v`);
     } else {
         console.log(` The ${this.name} can operate from the battery`)
     }
+
 }
 
-let webcam = new SmartHome('webcam', 24, 220, 'white');
+let webcam = new SmartHome('webcam', 'standart', 220, 'white', 'off');
 webcam.description();
 webcam.wifi_on();
