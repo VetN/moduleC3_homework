4707182bdd6e1e7632b0091344a15ae4ea36ee20
VetN <122635397+VetN@users.noreply.github.com>
2023-04-22 09:27:32 +0300

    creating a function-constructor

---
+++ 3.3.3.js
@@ -0,0 +1,38 @@
+/ЗАДАНИЕ 1
+//написатьь функцию, которая принимает в кач-ве аргумента объект
+// и выводит в консоль все ключи и значения только собственных св-в
+// данная функция не должна возвр значение
+
+let product = {
+    payment: "instalments",
+    guarantee: 24,
+    delivery: 'courier',
+};
+function furniture(product) {
+    for (let prop in product) {
+        if (product.hasOwnProperty(prop)) {
+            console.log(`key: ${prop}, value: ${product[prop]};`);
+        }
+    }
+        
+}
+furniture(product);
+
+// ЗАДАНИЕ 2
+//написать функцию, которая принимает строку и объект,
+// а затем проверяет есть ли у переданного объекта св-во
+// с данным именем. Функция возращ true или false
+function check(str, object) {
+    console.log(object)
+    return (str in object); {
+   }
+}
+console.log(check('payment', product));
+console.log(check('courier', product));
+console.log(check('delivery', product));
+
+// ЗАДАНИЕ 3
+// Написать функцию, которая создает пустой объект, без прототипа
+function createObject() {
+    return Object.create(null)
+}
---
+++ 3.4.4.js
@@ -0,0 +1,11 @@
+// создаем функцию- конструктор объектов
+function SmartHome() {
+    this.series = 'Comfort',
+    this.power = power,
+    this.color = color,
+    this.timer = timer,
+    this.accumulator = accumulator,
+    this.wi_fi = function () {
+        console.log(`connecting to wi-fi: ${wi_fi},`)
+    }          
+}
