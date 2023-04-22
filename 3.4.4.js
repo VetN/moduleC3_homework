acb814637e38fea5f5fe45f82de656104d5ff065
VetN <122635397+VetN@users.noreply.github.com>
2023-04-22 13:20:13 +0300

    корректировка вход данных

--- 3.4.4.js
+++ 3.4.4.js
@@ -20,13 +20,13 @@ SmartHome.prototype.wifi_off = function () {
 SmartHome.prototype.description = function () {
     console.log(`Product name: ${this.name}, series: ${this.series}`);
     console.log(`Product warranty: ${this.guarantee}, color ${this.color}`);
-    if (this.power === '220') {
+    if (this.power === 220) {
         console.log(`The ${this.name} is powered by 220v`);
     } else {
         console.log(` The ${this.name} can operate from the battery`)
     }
 }
 
-let webcam = new SmartHome('webcam', 24, 12, 'white');
+let webcam = new SmartHome('webcam', 24, 220, 'white');
 webcam.description();
 webcam.wifi_on();
