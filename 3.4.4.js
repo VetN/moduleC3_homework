01dc33cd20c0818a302d68f9bd8c1de8d260d2fd
VetN <122635397+VetN@users.noreply.github.com>
2023-04-23 11:57:00 +0300

    доделала функцию-конструктор

--- 3.4.4.js
+++ 3.4.4.js
@@ -1,36 +1,57 @@
 // создаем функцию- конструктор объектов
-function SmartHome(name, guarantee, power, color, wifi) {
+function SmartHome(name, power, color, wifi) {
     this.name = name,
     this.series = 'Comfort',
-    this.guarantee = guarantee,
+    this.ip = 'IP 44'
+    this.guarantee = 12,
     this.power = power,
     this.color = color,
-    this.wifi = wifi
+    this.wifi = wifi,
+    this.power_connected = true
 }
+
+// добавление методов в функцию-конструктор
+
 SmartHome.prototype.warranty = function () {
-    if (this.guarantee === 'standart') {
-        console.log(`${this.guarantee} warranty 12 month`)
-    } else if (this.guarantee === 'extended') {
-        console.log(`${this.guarantee} warranty 24 month`)
+    if (this.guarantee === 12 ) {
+        console.log(`Warranty: standart -  ${this.guarantee} month`)
+    } else if (this.guarantee === 24 ) {
+        console.log(`Warranty: extended - ${this.guarantee} month`)
     } else {
         console.log('Without warranty')
     }
 }
+
 SmartHome.prototype.wifi_on = function () {
    this.wifi == 'on' ?  console.log(`${this.name} connecting to wi-fi: on`) : console.log(`${this.name} connecting to wi-fi: off`);
 }  
 
 SmartHome.prototype.description = function () {
-    console.log(`Product name: ${this.name}, series: ${this.series}, color ${this.color}`);
+    console.log(`Product name: ${this.name}, series: ${this.series}, IP: ${this.ip}, color: ${this.color}`);
+    this.warranty();
     if (this.power === 220) {
         console.log(`The ${this.name} is powered by 220v`);
     } else {
         console.log(` The ${this.name} can operate from the battery`)
     }
-
+    this.wifi_on();
 }
 
-let webcam = new SmartHome('webcam', 'standart', 220, 'white', 'off') 
+SmartHome.prototype.power_connectedOn = function () {
+    this.power_connected = true;
+    console.log(`The ${this.name} is power connected`)
+}
+
+SmartHome.prototype.power_connectedOff = function () {
+    this.power_connected = false;
+    console.log(`The ${this.name} is not power connected`)
+}
+
+
+// Создаем объект с помощью функции-конструктора
+let webcam = new SmartHome('webcam', 220, 'white', 'off');
 webcam.description();
-webcam.warranty();
-webcam.wifi_on();
+webcam.power_connectedOn();
+console.log(webcam.color);
+
+
